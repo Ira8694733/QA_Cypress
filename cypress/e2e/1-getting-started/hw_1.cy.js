cc335f974bdc1c7bdc2ab03dcce6575e2996a7cd
Ira Galtsova <iragaltsova7@gmail.com>
2024-04-13 21:39:04 +0300

    I've completed the hw_1

--- cypress/e2e/1-getting-started/hw_1.cy.js
+++ cypress/e2e/1-getting-started/hw_1.cy.js
@@ -23,5 +23,11 @@ describe('Test quatro Sign In', () => {
         cy.get('input#signupPassword').type('123Q456w').should('have.value', '123Q456w');
         cy.get('input#signupRepeatPassword').type('123Q456w').should('have.value', '123Q456w');
         cy.get('button').contains('Register').click();
+
+        cy.url().should('include', '/panel/garage');
+
+        cy.get('button').contains('My profile').click();
+        cy.get('a.dropdown-item.btn.btn-link.user-nav_link').contains('Profile').click();
+        cy.get('.profile_name.display-4').should('have.text', 'TestNameZ');
     })
 });
