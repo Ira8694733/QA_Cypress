50dbf1b9ed375a7306c3aad700a84fca4dd06295
Ira Galtsova <iragaltsova7@gmail.com>
2024-04-18 19:47:41 +0300

    added tests and pages to hw_2

---
+++ cypress/pages/GarageProfile.js
@@ -0,0 +1,25 @@
+export default class GarageProfile {
+
+    addCar() {
+        return cy.xpath ("//button[contains(text(), 'Add car')]")
+        // ("//button[@class='btn btn-primary']");
+    }
+
+    addCarBrand() {
+        return cy.xpath("//select[@id='addCarBrand']");
+    }
+
+    addCarModel() {
+        return cy.xpath("//select[@id='addCarModel']")
+    }
+
+    addCarMileage() {
+        return cy.xpath("//input[@id='addCarMileage']");
+    }
+
+    add() {
+        return cy.xpath("//app-add-car-modal//button[.='Add']");
+    }
+}
+
+export const garageProfile = new GarageProfile()
