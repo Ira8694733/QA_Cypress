41cce4e95dedaaa82e6651002e66db12062b99dc
Ira Galtsova <iragaltsova7@gmail.com>
2024-04-18 19:47:41 +0300

    added tests and pages to hw_2

---
+++ cypress/pages/FuelExpenses.js
@@ -0,0 +1,28 @@
+export default class FuelExpenses {
+
+    addExpenses() {
+        return cy.xpath ("//button[contains(text(), 'Add an expense')]")
+    }
+
+    addCarExpenses() {
+        return cy.xpath("//input[@id='addCarExpenses']");
+    }
+
+    addExpenseMileage() {
+        return cy.xpath("//input[@id='addExpenseMileage']");
+    }
+
+    addExpenseLiters() {
+        return cy.xpath("//input[@id='addExpenseLiters']")
+    }
+
+    addExpenseTotalCost() {
+        return cy.xpath("//input[@id='addExpenseTotalCost']");
+    }
+
+    add() {
+        return cy.xpath("//app-add-expense-modal//button[.='Add']");
+    }
+}
+
+export const fuelExpenses = new FuelExpenses()
